66c852b2a20c22a05737f74dff33193d8d3ade88
ray omego <ray.omego@student.moringaschool.com>
2022-08-23 12:19:28 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function findMatching (names, name) {
+    const nameNoCase = name.toUpperCase();
+    const driverMatch = names.filter(elem => nameNoCase === elem.toUpperCase())
+    return driverMatch
+}
+
+function fuzzyMatch(names, letters) {
+    const endInd = letters.length
+    const driverMatch = names.filter(elem => elem.slice(0, endInd) === letters)
+    return driverMatch
+}
+
+function matchName(names, string) {
+    const driverMatch = names.filter(elem => elem.name === string);
+    return driverMatch
+}
